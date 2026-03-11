c1b5d3cbd03821fec6a2c30d7b68e395bb3e24f6
Ramesh K R <rameshkr423@gmail.com>
2026-03-11 08:13:33 +0530

    terraform file

--- terraform/terraform.tf
+++
@@ -1,6 +0,0 @@
-@'
-project_id  = "banking-system-prod"
-region      = "us-west1"
-zone        = "us-west1-a"
-db_password = "Banking@Secure123"
-'@ | Out-File -FilePath "terraform.tfvars" -Encoding UTF8
